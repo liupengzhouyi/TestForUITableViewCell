af4b27e2748ea5fb936af1a28b03e41953937564
liupeng <LIUPENG.0@outlook.com>
2018-10-09 22:39:47 +0800

    Initial Commit

---
+++ TestForUITableViewCell/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestForUITableViewCell
+//
+//  Created by 刘鹏 on 2018/10/9.
+//  Copyright © 2018 刘鹏. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
